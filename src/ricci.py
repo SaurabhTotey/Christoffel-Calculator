63203258361390d5446dec08411ee0b3a34ef6f7
Saurabh Totey <SaurabhTotey@gmail.com>
2023-04-11 13:38:15 -0600

    Simplify Ricci Scalar calculation

--- src/ricci.py
+++ src/ricci.py
@@ -1,10 +1,7 @@
 from sage.all import *
 from . import christoffel, riemann
 
-def calculate_ricci_tensor(metric_with_lower_indices, variables, lower_variable_one, lower_variable_two):
-	christoffels = christoffel.calculate_all_christoffel_symbols(metric_with_lower_indices, variables)
-	lower_index_one = variables.index(lower_variable_one)
-	lower_index_two = variables.index(lower_variable_two)
+def calculate_single_ricci_tensor_with_all_connection_coefficients(christoffels, variables, lower_index_one, lower_index_two):
 	return sum([
 		riemann.calculate_single_riemann_tensor_with_all_connection_coefficients(
 			christoffels,
@@ -16,6 +13,13 @@ def calculate_ricci_tensor(metric_with_lower_indices, variables, lower_variable_
 		) for i in len(range(variables))
 	])
 
+def calculate_ricci_tensor(metric_with_lower_indices, variables, lower_variable_one, lower_variable_two):
+	return calculate_single_ricci_tensor_with_all_connection_coefficients([
+		christoffel.calculate_all_christoffel_symbols(metric_with_lower_indices, variables),
+		variables.index(lower_variable_one),
+		variables.index(lower_variable_two)
+	])
+
 def calculate_all_ricci_tensors(metric_with_lower_indices, variables):
 	all_riemann_tensors = riemann.calculate_all_riemann_tensors(metric_with_lower_indices, variables)
 	all_ricci_tensors = [[None for _ in range(len(variables))] for _ in range(len(variables))]
@@ -25,10 +29,9 @@ def calculate_all_ricci_tensors(metric_with_lower_indices, variables):
 	return all_ricci_tensors
 
 def calculate_ricci_scalar(metric_with_lower_indices, variables):
-	metric_with_upper_indices = metric_with_lower_indices.inverse()
-	ricci_tensors = calculate_all_ricci_tensors(metric_with_lower_indices, variables)
+	christoffels = christoffel.calculate_all_christoffel_symbols(metric_with_lower_indices, variables)
 	return sum([
-		metric_with_upper_indices[i][j] * ricci_tensors[i][j] for i in range(len(variables)) for j in range(len(variables))
+		calculate_single_ricci_tensor_with_all_connection_coefficients(christoffels, variables, i, i) for i in range(len(variables))
 	])
 
 if __name__ == "__main__":
